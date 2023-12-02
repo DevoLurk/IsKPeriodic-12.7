b9ed9c7f03354b304c225c928eb46a25a3a1b3d0
Devo <145495048+DevoLurk@users.noreply.github.com>
2023-12-02 15:37:08 +0300

    second

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ bool IsKPeriodic(string& pat, int& period)
 {
     int j = 0;
     int i = 1;
-    int part_len = 1;
+    int part_len = 0;
 
     int* lps = new int[pat.length()];
     lps[0] = 0; 
@@ -19,25 +19,36 @@ bool IsKPeriodic(string& pat, int& period)
         else
         {
             if (j != 0) 
-            {
                 j = lps[j - 1];
-                return false;
-            }
             else
-            {
                 lps[i++] = 0;
-                part_len++;
-            }
+            
         }
     }
 
-   /* cout << "pat len = " << pat_len << endl;
-    cout << "str len = " << pat.length() << endl;
-    cout << "lps end = " << lps[pat.length() - 1] << endl;
-    cout << endl;*/
+
+    for (auto i{ pat.length()  - 1 }; i > 1; i--)
+    {
+        if (lps[i] > lps[i - i])
+            part_len++;
+    }
 
 
-    period = lps[pat.length() - 1] / part_len;
+
+    /*for (auto i{ 0 }; i < pat.length(); i++)
+    {
+        cout << " " << lps[i];
+    }
+    cout << endl;
+
+    cout << "str   len = " << pat.length() << endl;
+    cout << "end   len = " << part_len << endl;
+    cout << "start len = " << pat.length() - part_len << endl;
+    cout << "lps   end = " << lps[pat.length() - 1] << endl;
+    cout << endl; */
+
+
+    period = lps[pat.length() - 1] / (pat.length() - part_len);
     return true;
 }
 
@@ -48,7 +59,7 @@ int main()
     string test1 = "ABCABCD";
     string test2 = "AABAACAABAA";
     string test3 = "asdasdasdasd";
-    string test4 = "asdfasdfasdfasdfd";
+    string test4 = "ffffaaffffaaffffaa";
 
     if (IsKPeriodic(test4, period))
         cout << "period = " << period << endl;
