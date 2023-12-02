886c130d9eb985b9e961803446a9c8b93e854043
Devo <145495048+DevoLurk@users.noreply.github.com>
2023-12-02 19:55:09 +0300

    rebuild

--- main.cpp
+++ main.cpp
@@ -3,53 +3,27 @@
 
 using namespace std;
 
-bool IsKPeriodic(string& pat, int& period)
+bool IsKPeriodic(string& str, int period)
 {
-    int j = 0;
-    int i = 1;
-    int part_len = 0;
+    if (!period)
+        return false;
+    if ((str.length() % period) != 0)
+        return false;
 
-    int* lps = new int[pat.length()];
-    lps[0] = 0; 
-
-    while (i < pat.size())
-    {
-        if (pat[i] == pat[j]) 
-            lps[i++] = ++j;
-        else
-        {
-            if (j != 0) 
-                j = lps[j - 1];
-            else
-                lps[i++] = 0;
-            
-        }
-    }
-
-
-    for (auto i{ pat.length()  - 1 }; i > 1; i--)
-    {
-        if (lps[i] > lps[i - i])
-            part_len++;
-    }
-
-
-
-    /*for (auto i{ 0 }; i < pat.length(); i++)
-    {
-        cout << " " << lps[i];
-    }
-    cout << endl;
-
-    cout << "str   len = " << pat.length() << endl;
-    cout << "end   len = " << part_len << endl;
-    cout << "start len = " << pat.length() - part_len << endl;
-    cout << "lps   end = " << lps[pat.length() - 1] << endl;
-    cout << endl; */
-
-
-    period = lps[pat.length() - 1] / (pat.length() - part_len);
+    for (int i = period; i < str.length(); i += period)
+        for (int j = 0; j < period; j++)
+            if (str[j] != str[period + j])
+                return false;
     return true;
+}
+
+void print(string& str)
+{
+    for (auto i{ 0 }; i < (str.length() / 2) + 1; i++)
+        if (IsKPeriodic(str, i))
+            printf(" %s k = %3d \033[032mtrue\033[0m\n", str.c_str(), i);
+        else
+            printf(" %s k = %3d \033[031mfalse\033[0m\n", str.c_str(), i);
 }
 
 
@@ -59,12 +33,10 @@ int main()
     string test1 = "ABCABCD";
     string test2 = "AABAACAABAA";
     string test3 = "asdasdasdasd";
-    string test4 = "ffffaaffffaaffffaa";
+    string test4 = "abcabcabcabcabcabcabcabc";
 
-    if (IsKPeriodic(test4, period))
-        cout << "period = " << period << endl;
-    else
-        cout << "str is not periodik" << endl;
+
+    print(test3);
 
     return 0;
 }
